bff644eb469961d8d0075ba1dd9a47160cbad101
Bhushan Kumar <bhushankumarjnr1@gmail.com>
2022-08-30 16:54:39 +0530

    Main modified

--- data_conn/src/app.service.ts
+++ data_conn/src/app.service.ts
@@ -1,38 +1,17 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Client, ClientProxy, MqttRecordBuilder, Transport } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy, MqttRecordBuilder } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('TEST_CLIENT') private client: ClientProxy
+    ) {}
+
+  sumDataService(payload: number[]) {
+    const response = payload.reduce((a, b) => a + b, 0);
+    const record = new MqttRecordBuilder(`${response}`).setQoS(1).build();
+    this.client.send('ftf_output', record).subscribe((res) => {
+      console.log('response output: <', res, '>');
+    });
   }
-  // sumDataService(Payload:number[]){
-  //   const response = Payload.reduce((a,b) => a+b , 0);
-  //   const record = new MqttRecordBuilder(`${response}`)
-  //   .setQoS(0)
-  //   .build();
-  //   this.client.send("FTF-input",record).subscribe(res => {
-  //     console.log('response output',res,'>')
-  //   })
-  // }
-  @Client({ transport: Transport.MQTT })
-
-  public client: ClientProxy; 
-
-  public async onModuleInit(): Promise<void> {
-    Logger.log("Connecting");
-    // await this.client.connect();
-    Logger.log("Connected");
-    // Logger.log("time for delay");
-  }
-
-  public sendMessage(): Observable<number> {
-    const pattern: {} = { cmd: "sum" };
-    const data: number[] = [5, 6];
-
-    return this.client.send<number>(pattern, data);
-  }
-
-
 }
